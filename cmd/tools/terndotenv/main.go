dfcc752b93759080f89f7726d1961cc5013f829f
Gio <105086221+giovannabreinack@users.noreply.github.com>
2024-08-09 17:35:15 -0300

    Update main.go

--- cmd/tools/terndotenv/main.go
+++ cmd/tools/terndotenv/main.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+	"os/exec"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	cmd := exec.Command(
+		"tern",
+		"migrate",
+		"--migrations",
+		"./internal/store/migrations",
+		"--config",
+		"./internal/store/migrations/tern.conf",
+	)
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+}
